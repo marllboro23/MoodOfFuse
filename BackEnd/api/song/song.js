c9aad16a59026a329dc3183de4c3332d74d5dade
marllboro23 <donaldnam1995@gmail.com>
2019-04-26 15:33:25 +0800

    .

---
+++ BackEnd/api/song/song.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const songModel = require('./songModel');
+
+const SongRouter = express.Router();
+
+SongRouter.get("/get",(req,res) => {
+    const SongData = req.body;
+    songModel.find({}, (err, data) => {
+        if(err) console.log("jafufhblbwbi")
+        else console.log("success");
+        res.send(data);
+    });
+});
+
+// SongRouter.delete("/delete", (req,res) =>{
+//     co
+// })
+
+// SongRouter.post("")
+
+
+module.exports = SongRouter;
